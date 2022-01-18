6d1a1fad4d91bbe9e564d1aa9e4489a140a7df5b
Luan Rafael <luanraffaell@hotmail.com>
2022-01-18 19:52:51 -0200

    Aplicativo para cadastro e atualizacao e filmes em memoria

---
+++ Series/Classes/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Series.Classes
+{
+    class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
